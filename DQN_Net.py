3eadd9bef64be86115bb1c3ab18ec89dcbaf7434
Dennis Lehmann <dennis.d.lehmann@gmail.com>
2021-02-15 12:21:20 +0100

    first Net

--- DQN_Net.py
+++ DQN_Net.py
@@ -6,5 +6,16 @@ import torchvision.transforms as T
 
 
 class DQN(nn.Module):
-    def __init__(self):
+    def __init__(self, img_shape: int, num_actions: int, learning_rate: float):
         super().__init__()
+        self.img_shape = img_shape
+        self.num_actions = num_actions
+        self.learning_rate = learning_rate
+        self.internal_model = self.build_model()
+
+    def build_model(self):
+        conv_1 = nn.Conv2d(self.img_shape, 32, 8, 4, 4)
+        conv_2 = nn.Conv2d(32, 64, 4, 2, 2)
+        conv_3 = nn.Conv2d(64, 64, 3, 1, 1)
+        relu = nn.ReLU()
+        line = nn.Linear(64, 256)
